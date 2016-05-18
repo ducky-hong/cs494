b5053b02d4a5d43d76b5f69f30d4cadf86015ff5
ducky-hong <pacifings@gmail.com>
2016-05-19 00:57:07 +0900

    Fix connection issue & Add nbase-arc

--- app.rb
+++ app.rb
@@ -9,10 +9,11 @@ ActiveRecord::Base.establish_connection(:development)
 class User < ActiveRecord::Base; end
 
 arcus = Dalli::Client.new("#{ENV['ARCUS_HOST']}:11211")
-nbase_arc = Redis.new(host: "#{ENV['NBASE_ARC_HOST']}", port: 6379)
+nbase_arc = Redis.new(host: "#{ENV['NBASE_ARC_HOST']}", port: 6000)
 
 get '/mysql' do
   user = User.find(1)
+  User.clear_all_connections!
   user.to_json
 end
 
@@ -20,6 +21,7 @@ get '/arcus' do
   user_cached = arcus.get('user:1')
   if user_cached.nil?
     user_json = User.find(1).to_json
+    User.clear_all_connections!
     arcus.set('user:1', user_json)
     user_json
   else
@@ -33,6 +35,7 @@ get '/nbase-arc' do
   user_cached = nbase_arc.get('user:1')
   if user_cached.nil?
     user_json = User.find(1).to_json
+    User.clear_all_connections!
     nbase_arc.set('user:1', user_json)
     user_json
   else
